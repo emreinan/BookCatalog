3d00134165011acb1d4af91d64acc67dbed6cb52
Emre İnan <emreinannn@gmail.com>
2024-12-11 17:43:26 +0300

    feat: SQLite impelemted with migration.

---
+++ BookCatalog.Data/Entity/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookCatalog.Data.Entity
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int PageCount { get; set; }
+    }
+}
---
+++ BookCatalog/MvcExtenisons.cs
@@ -0,0 +1,26 @@
+﻿using BookCatalog.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
+
+namespace BookCatalog;
+
+public static class MvcExtenisons
+{
+    public static void ConfigureSerilog(this IHostBuilder hostBuilder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .WriteTo.File("logs.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        hostBuilder.UseSerilog();
+    }
+
+    public static IServiceCollection ConfigureDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+        return services;
+    }
+}
