6eccb36c1812895ebb683b2217ff91005a902745
Emre İnan <emreinannn@gmail.com>
2024-12-17 12:37:04 +0300

    feat: Entity Configuration implemented.

--- BookCatalog.Data/Entity/Book.cs
+++ BookCatalog.Data/Entity/Book.cs
@@ -1,4 +1,7 @@
-﻿namespace BookCatalog.Data.Entity
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookCatalog.Data.Entity
 {
     public class Book
     {
@@ -7,5 +10,30 @@
         public string Author { get; set; }
         public string Genre { get; set; }
         public int PageCount { get; set; }
+    }
+
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.ToTable("Books");
+
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Id).ValueGeneratedOnAdd();
+            builder.Property(b => b.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(b => b.Author)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(b => b.Genre)
+                .HasMaxLength(50);
+
+            builder.Property(b => b.PageCount)
+                .IsRequired();
+        }
     }
 }
